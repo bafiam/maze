301af649c5a1e53d95b065fb41dee510b0dcefae
bafiam <stevebafiam@gmail.com>
2020-11-22 22:32:39 +0300

    ft: add board, height  and width form and play

--- src/App.js
+++ src/App.js
@@ -1,23 +1,179 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import _ from "lodash";
+const rand = () => {
+  return Math.round(Math.random() * 20) - 10;
+};
+
+const getModalStyle = () => {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+};
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  userForm: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [gridWidth, setGridWidth] = useState(1);
+  const [gridHeight, setGridHeight] = useState(1);
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+  const [resp, setResp] = useState(" ");
+  const [avatar, setAvatar] = useState("");
+  const [splits, setSplits] = useState([]);
+
+  const gameBoard = (w, h) => {
+    let board = [];
+    for (let idxH = 1; idxH <= h; idxH++) {
+      for (let idxW = 1; idxW <= w; idxW++) {
+        let comb = `${idxH}${idxW}`;
+        board.push(comb);
+      }
+    }
+
+    return board;
+  };
+  const avatarPosition = (w, h) => {
+    let widthMid;
+    let heightMid;
+    if (h === 1) {
+      let newHeight = h + 1;
+      heightMid = Math.floor(newHeight / 2);
+    } else {
+      heightMid = Math.floor(h / 2);
+    }
+    if (w === 1) {
+      let newWidth = w + 1;
+      widthMid = Math.floor(newWidth / 2);
+    } else {
+      widthMid = Math.floor(w / 2);
+    }
+
+    return `${heightMid}${widthMid}`;
+  };
+
+  const handleOpen = () => {
+    setAvatar(avatarPosition(gridWidth, gridHeight));
+    setGridWidth(1);
+    setGridHeight(1);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setAvatar(avatarPosition(gridWidth, gridHeight));
+    setOpen(false);
+    let board = gameBoard(gridWidth, gridHeight);
+    let newBoard = [...board];
+    _.remove([...newBoard], (n) => n == avatar);
+    setSplits(_.sampleSize(newBoard, gridHeight));
+  };
+ 
+  const handleHeightChange = (e) => {
+    if (e.target.value >= 1 && e.target.value < 10) {
+      setGridHeight(e.target.value);
+    } else {
+      setResp(<p>Height should be between 1 and 9</p>);
+    }
+  };
+  const handleWidthChange = (e) => {
+    if (e.target.value >= 1 && e.target.value < 10) {
+      setGridWidth(e.target.value);
+    } else {
+      setResp(<p>Width should be between 1 and 9</p>);
+    }
+  };
+  const play = () => {
+    let getAvatar = document.getElementById(`${avatar}`);
+    console.log(getAvatar);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Input board height and width:</h2>
+      {resp}
+      <form className={classes.userForm} noValidate autoComplete="off">
+        <TextField
+          id="standard-basic"
+          label="Height"
+          onChange={handleHeightChange}
+        />
+        <TextField
+          id="standard-basic"
+          label="Width"
+          onChange={handleWidthChange}
+        />
+      </form>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`root`}>
+      <div className={`App`}>
+        <div className={`user-form`}>
+          <Button
+            type="button"
+            variant="contained"
+            color="secondary"
+            onClick={handleOpen}
+          >
+            Input board height and width
+          </Button>
+          <Button
+            type="button"
+            variant="contained"
+            color="primary"
+            onClick={play}
+          >
+            Play
+          </Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            {body}
+          </Modal>
+        </div>
+        {Array.from({ length: gridHeight }, (_, h) => h + 1).map((hValue) => (
+          <Grid key={hValue} container spacing={1} direction="row">
+            {Array.from({ length: gridWidth }, (_, i) => i + 1).map((value) => (
+              <Grid key={value} item>
+                <Paper className={`cell`} id={`${hValue}${value}`}>
+                  <span className={`plain`}>{`${hValue}${value}`}</span>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
+        ))}
+      </div>
     </div>
   );
 }
